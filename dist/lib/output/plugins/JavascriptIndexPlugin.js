1ff81d2f7cf4dedbcdc87001986e3437cf52bdbc
Gabriel Vargas <gabrielfvf@outlook.com>
2019-11-28 16:41:31 -0300

    a mudança da tag q esqueci

--- dist/lib/output/plugins/JavascriptIndexPlugin.js
+++ dist/lib/output/plugins/JavascriptIndexPlugin.js
@@ -2,23 +2,41 @@
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+            ({
+                    __proto__: []
+                }
+                instanceof Array && function (d, b) {
+                    d.__proto__ = b;
+                }) ||
+            function (d, b) {
+                for (var p in b)
+                    if (b.hasOwnProperty(p)) d[p] = b[p];
+            };
         return extendStatics(d, b);
     }
     return function (d, b) {
         extendStatics(d, b);
-        function __() { this.constructor = d; }
+
+        function __() {
+            this.constructor = d;
+        }
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
-    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
-    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
-    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    var c = arguments.length,
+        r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc,
+        d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators,
+        target, key, desc);
+    else
+        for (var i = decorators.length - 1; i >= 0; i--)
+            if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-Object.defineProperty(exports, "__esModule", { value: true });
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
 var Path = require("path");
 var index_1 = require("../../models/reflections/index");
 var GroupPlugin_1 = require("../../converter/plugins/GroupPlugin");
@@ -27,6 +45,7 @@ var fs_1 = require("../../utils/fs");
 var events_1 = require("../events");
 var JavascriptIndexPlugin = (function (_super) {
     __extends(JavascriptIndexPlugin, _super);
+
     function JavascriptIndexPlugin() {
         return _super !== null && _super.apply(this, arguments) || this;
     }
@@ -51,10 +70,17 @@ var JavascriptIndexPlugin = (function (_super) {
             if (parent_1 instanceof index_1.ProjectReflection) {
                 parent_1 = null;
             }
+            //crias as rows
             var row = {
                 id: rows.length,
                 kind: reflection.kind,
-                name: reflection.name,
+                //realiza a busca aqui
+                name: reflection.name + (reflection.comment ?
+                    (reflection.comment.text.length === 0 ? ' (' + reflection.comment.shortText
+                        .split('<')
+                        .pop().split('>')[0] + ')' :
+                        ' (' + reflection.comment.text.split('<')
+                        .pop().split('>')[0]) : ''),
                 url: reflection.url,
                 classes: reflection.cssClasses
             };
@@ -67,11 +93,18 @@ var JavascriptIndexPlugin = (function (_super) {
             rows.push(row);
         }
         var fileName = Path.join(event.outputDirectory, 'assets', 'js', 'search.js');
-        var data = "var typedoc = typedoc || {};\n            typedoc.search = typedoc.search || {};\n            typedoc.search.data = " + JSON.stringify({ kinds: kinds, rows: rows }) + ";";
+        var data =
+            "var typedoc = typedoc || {};\n            typedoc.search = typedoc.search || {};\n            typedoc.search.data = " +
+            JSON.stringify({
+                kinds: kinds,
+                rows: rows
+            }) + ";";
         fs_1.writeFile(fileName, data, false);
     };
     JavascriptIndexPlugin = __decorate([
-        components_1.Component({ name: 'javascript-index' })
+        components_1.Component({
+            name: 'javascript-index'
+        })
     ], JavascriptIndexPlugin);
     return JavascriptIndexPlugin;
 }(components_1.RendererComponent));
